b32f169d114bb00bc94da1091aa2c113817451ac
DvrDania <dvr.dania@gmail.com>
2021-09-26 11:23:09 +0330

    adding new album functionality implemented

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,24 @@ extern crate rocket;
 use rocket::response::status;
 use rocket::serde::json::{json, Json, Value};
 use rocket::State;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 mod albums;
 use albums::Album;
 
 #[get("/albums")]
-fn get_albums(albums: &State<Vec<Album>>) -> Json<Vec<Album>> {
-    Json(albums.to_vec())
+fn get_albums(albums: &State<Arc<Mutex<Vec<Album>>>>) -> Json<Vec<Album>> {
+    Json(albums.lock().unwrap().to_vec())
 }
 
 #[get("/albums/<id>")]
 fn get_single_album(
     id: u32,
-    albums: &State<Vec<Album>>,
-) -> Result<Json<&Album>, status::NotFound<Value>> {
-    for album in albums.iter() {
+    albums: &State<Arc<Mutex<Vec<Album>>>>,
+) -> Result<Json<Album>, status::NotFound<Value>> {
+    let albums = albums.lock().unwrap().to_vec();
+    for album in albums {
         if id == album.id {
             return Ok(Json(album));
         }
@@ -26,12 +29,14 @@ fn get_single_album(
     Err(status::NotFound(json!({ "message": "album not found" })))
 }
 
-#[post("/albums")]
-fn add_album() {}
+#[post("/albums", format = "json", data = "<new>")]
+fn add_album(new: Json<Album>, albums: &State<Arc<Mutex<Vec<Album>>>>) {
+    albums.lock().unwrap().push(new.into_inner());
+}
 
 #[launch]
 fn rocket() -> _ {
-    let albums = albums::dummy();
+    let albums = Arc::from(Mutex::from(albums::dummy()));
     rocket::build()
         .manage(albums)
         .mount("/", routes![get_albums, get_single_album, add_album])
